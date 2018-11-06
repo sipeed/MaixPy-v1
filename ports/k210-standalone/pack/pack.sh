4a722176e51a7a30a48f73c98dd6d17405a81afa
xiahui <2535418266@qq.com>
2018-11-06 13:09:56 +0800

    add pack bin and pack kfpkg

--- ports/k210-standalone/pack/pack.sh
+++ ports/k210-standalone/pack/pack.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+bsize='1M'
+bcount=12
 cd bin_file
-zip -r ./firmware.zip ./* 
-mv firmware.zip ../sipeedm1.kfpkg
+python3 ../pack/pre_process_mpy.py
+dd if=/dev/zero of=firmware.bin bs=$bsize count=$bcount
+dd if=micropython_sha256.bin of=firmware.bin conv=notrunc
+cfgseek=$[0x80000]
+echo  readl cfgseek is $[$cfgseek]
+dd if=cfg.bin of=firmware.bin bs=1 seek=$[$cfgseek] conv=notrunc
+modseek=$[0x90000]
+echo  real modseek is $[$modseek-5]
+dd if=model.bin of=firmware.bin bs=1 seek=$[$modseek] conv=notrunc
+fsseek=$[0x600000]
+echo  real fsseek is $[$fsseek-5]
+dd if=fs.bin of=firmware.bin bs=1 seek=$[$fsseek] conv=notrunc
+rm -rf micropython_sha256.bin
