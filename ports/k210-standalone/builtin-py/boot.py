12e3c0afc211d58408f9d744ba453c46a1dab542
wipping <wiiping@163.com>
2018-11-15 01:44:29 -0800

    add fpioa manager

--- ports/k210-standalone/builtin-py/boot.py
+++ ports/k210-standalone/builtin-py/boot.py
@@ -5,7 +5,9 @@ import machine
 import platform
 import app
 import board
+import fpioa_manager
 board_info=board.board_info()
+fm=fpioa_manager.fpioa_manager()
 file_list = os.ls()
 for i in range(len(file_list)):
     if file_list[i] == '/init.py':
