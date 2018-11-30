6f6e007a0e1cc45a2a03135d9d779a1c12e08360
wipping <wiiping@163.com>
2018-11-29 23:19:51 -0800

    add ctrl+c

--- ports/k210-standalone/mphalport.h
+++ ports/k210-standalone/mphalport.h
@@ -7,7 +7,7 @@
 
 static inline mp_uint_t mp_hal_ticks_ms(void) { return 0; }
 //static inline void mp_hal_set_interrupt_char(int c) {}
-
+void mp_hal_set_interrupt_char(int c);
 void mp_hal_delay_us(mp_uint_t us);
 void mp_hal_delay_ms(mp_uint_t ms);
 
