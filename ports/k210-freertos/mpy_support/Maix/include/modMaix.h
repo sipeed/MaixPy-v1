001e6a90c5444c609448f06384e8d53f41e03a06
wipping <wiiping@163.com>
2018-12-20 01:18:36 -0800

    add modMaix include dir

---
+++ ports/k210-freertos/mpy_support/Maix/include/modMaix.h
@@ -0,0 +1,8 @@
+#ifndef MICROPY_INCLUDED_MAIX_MAIX_H
+#define MICROPY_INCLUDED_MAIX_MAIX_H
+
+#include "py/obj.h"
+
+extern const mp_obj_type_t Maix_fpioa_type;
+
+#endif // MICROPY_INCLUDED_MAIX_MAIX_H
