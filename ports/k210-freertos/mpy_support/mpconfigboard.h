5d9db0039ba1db4e33bd7d32062b963c990cfebc
Neucrack <czd666666@gmail.com>
2019-01-14 19:21:24 +0800

    merge branch

--- ports/k210-freertos/mpy_support/mpconfigboard.h
+++ ports/k210-freertos/mpy_support/mpconfigboard.h
@@ -12,24 +12,7 @@
 #define ESP8285_BUF_SIZE 2048 
 #define MICROPY_UARTHS_DEVICE 4
 #define MICROPY_UART_NIC 1
-typedef struct _machine_uart_obj_t {
-    mp_obj_base_t base;
-	uint8_t uart_num;
-    uint32_t baudrate;
-    uint8_t bitwidth;
-    uart_parity_t parity;
-    uart_stopbit_t stop;
-	bool active : 1;
-	bool attached_to_repl: 1;
-	bool rx_int_flag: 1;
-	uint16_t read_buf_len;             
-    uint16_t read_buf_head;    
-    uint16_t read_buf_tail;
-	uint16_t timeout;
-	uint16_t timeout_char;
-	uint16_t data_len;
-    uint8_t *read_buf;
-} machine_uart_obj_t;
+
 typedef struct _ipconfig_obj
 {
 	mp_obj_t ip;
