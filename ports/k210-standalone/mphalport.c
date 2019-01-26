c039821a3b52d4e73492251183495e14bf85cb14
hh <robert@hammelrath.com>
2019-01-26 13:35:56 +0100

    modutime.c, ..: Enable the ticks_xxx() methods
    Which include ticks_ms, ticks_us, ticks_cpu, ticks_diff, ticks_add
    
    The ticks values will overflow at 2**62, so it is not likely to happen.
    Still ticks_diff and ticks_add are able to deal with that.

--- ports/k210-standalone/mphalport.c
+++ ports/k210-standalone/mphalport.c
@@ -8,6 +8,9 @@
 #include "mphalport.h"
 
 #include "sleep.h"
+#include "sysctl.h"
+#include "encoding.h"
+
 void mp_hal_delay_ms(mp_uint_t ms) {
 	msleep(ms);
 }
@@ -15,4 +18,17 @@ void mp_hal_delay_ms(mp_uint_t ms) {
 void mp_hal_delay_us(mp_uint_t us) {
 	usleep(us);
 }
+
+mp_uint_t mp_hal_ticks_us(void) {
+    return (read_cycle()) / (sysctl_clock_get_freq(SYSCTL_CLOCK_CPU) / 1000000UL);
+}
+
+mp_uint_t mp_hal_ticks_ms(void) {
+    return (read_cycle()) / (sysctl_clock_get_freq(SYSCTL_CLOCK_CPU) / 1000UL);
+}
+
+mp_uint_t mp_hal_ticks_cpu(void) {
+    return (read_cycle());
+}
+
 
--- ports/k210-standalone/mphalport.h
+++ ports/k210-standalone/mphalport.h
@@ -5,7 +5,6 @@
 #include "py/ringbuf.h"
 #include "lib/utils/interrupt_char.h"
 
-static inline mp_uint_t mp_hal_ticks_ms(void) { return 0; }
 //static inline void mp_hal_set_interrupt_char(int c) {}
 void mp_hal_set_interrupt_char(int c);
 void mp_hal_delay_us(mp_uint_t us);
