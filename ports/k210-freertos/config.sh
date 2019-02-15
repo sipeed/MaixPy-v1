f5ccd5a48bfee2624b8f3a2f7ddb60b79cdaae27
Neucrack <czd666666@gmail.com>
2019-02-15 17:19:38 +0800

    optimize compile

--- ports/k210-freertos/config.sh
+++ ports/k210-freertos/config.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-export PROJECT_DIR=/media/neucrack/data/main/work/sipeed/k210/code/MaixPy_Internal/ports/k210-freertos
-export TOOLCHAIN_PATH=/media/neucrack/data/main/work/sipeed/k210/tools/toolchain/kendryte-toolchain
+
+config_file=config.conf
+if [ ! -f $config_file ]; then
+    echo "Please config toolchain path first in config.conf"
+    echo "toolchain_path=/opt/kendryte-toolchain" > config.conf
+    exit 1
+fi
+build_config=`cat $config_file`
+toolchain_setting=`echo ${build_config} |grep toolchain_path`
+toolchain_path="/"`echo -e ${toolchain_setting#*/}`
+
+if [ ! -d $toolchain_path ]; then
+    echo "can not find toolchain, please set toolchain path in config.conf"
+    echo "toolchain_path=/opt/kendryte-toolchain/bin" > config.conf
+    exit 1
+fi
+
+cwd=`pwd`
+export PROJECT_DIR=$cwd
+export TOOLCHAIN_PATH=${toolchain_path}
 export PLATFORM=k210
 export BIN_DIR=`pwd`/output
 export INC_DIR=`pwd`/inc
-export SHELL=/bin/bash
 
-# don't edit
+export SHELL=/bin/bash
 export PLATFORM_MK=`pwd`/${PLATFORM}.mk
 export INCLUDE_MK=include.mk
 export COMMON_MK=`pwd`/common.mk
@@ -15,4 +32,8 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${TOOLCHAIN_PATH}/bin
 export CROSS_COMPILE=${TOOLCHAIN_PATH}/bin/riscv64-unknown-elf-
 export TOOLCHAIN_LIB_DIR=${TOOLCHAIN_PATH}/lib/gcc/riscv64-unknown-elf/8.2.0
 
-echo "config ok!"
+echo "=============================="
+echo "toolchain path: $TOOLCHAIN_PATH"
+echo "k210 port path: $PROJECT_DIR"
+echo "=============================="
+
