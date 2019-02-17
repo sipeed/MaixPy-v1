5a0ce246223f17c8c3750041185cfd76de430858
Neucrack <czd666666@gmail.com>
2019-02-17 09:25:59 +0800

    fix demo and optimize startup script

--- ports/k210-freertos/mpy_support/builtin-py/_boot.py
+++ ports/k210-freertos/mpy_support/builtin-py/_boot.py
@@ -8,4 +8,10 @@ from fpioa_manager import *
 sys.path.append('flash')
 board_info=board_info()
 
+devices = os.listdir("/")
+if "sd" in devices:
+    os.chdir("/sd")
+else:
+    os.chdir("/flash")
+
 
