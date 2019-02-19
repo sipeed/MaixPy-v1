73f84743fe17676b55ac45abce6ac5ca621483ea
Neucrack <czd666666@gmail.com>
2019-02-19 15:24:31 +0800

    change fpioa arg keyword

--- ports/k210-freertos/mpy_support/Maix/Maix_fpioa.c
+++ ports/k210-freertos/mpy_support/Maix/Maix_fpioa.c
@@ -27,16 +27,16 @@ const mp_obj_type_t Maix_fpioa_type;
 STATIC mp_obj_t Maix_set_function(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
 	enum {
 		ARG_pin,
-		ARG_fun,
+		ARG_func,
 	};
 	static const mp_arg_t allowed_args[] = {
 		{ MP_QSTR_pin, 	MP_ARG_INT, {.u_int = 0} },
-		{ MP_QSTR_fun,	MP_ARG_INT, {.u_int = 0} },
+		{ MP_QSTR_func,	MP_ARG_INT, {.u_int = 0} },
 	};
 	mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all(n_args-1, pos_args+1, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
 	uint16_t pin_num = args[ARG_pin].u_int;
-	fpioa_function_t func_num = args[ARG_fun].u_int;
+	fpioa_function_t func_num = args[ARG_func].u_int;
 	
 	if(pin_num < 0 || pin_num > FPIOA_NUM_IO)		
 		mp_raise_ValueError("Don't have this Pin");
@@ -55,15 +55,15 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_KW(Maix_set_function_obj, 0,Maix_set_function);
 
 STATIC mp_obj_t Maix_get_Pin_num(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
 	enum {
-		ARG_fun,
+		ARG_func,
 	};
 	static const mp_arg_t allowed_args[] = {
-		{ MP_QSTR_fun,	MP_ARG_INT, {.u_int = 0} },
+		{ MP_QSTR_func,	MP_ARG_INT, {.u_int = 0} },
 	};
 	mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all(n_args-1, pos_args+1, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
 	
-	fpioa_function_t fun_num = args[ARG_fun].u_int;
+	fpioa_function_t fun_num = args[ARG_func].u_int;
 	
 	if(fun_num < 0 || fun_num > USABLE_FUNC_NUM)
 		mp_raise_ValueError("This function is invalid");
