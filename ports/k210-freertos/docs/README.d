56e5d53d3f98d07b607fbdcbf43248566e146432
Neucrack <czd666666@gmail.com>
2019-03-06 18:21:30 +0800

    delete bin doc files..

--- ports/k210-freertos/docs/README.d
+++
@@ -1,7 +0,0 @@
-# MAIXPY FOR SIPEED_M1 FREERTOS
-## Does not support multi-threaded compilation
-## something test log in http://blog.sipeed.com/p/175.html
-## Verify that each module's modifications are compiled successfully and connected to the output file
-## Please manually add the used header file path at the beginning of each layer of Makefile.
-## The Makefile in the mpy_support directory will search all source code files (including subdirectories) in the current directory.
-## Verifying that the cpp source code is compiled properly, and that the cpp feature is supported normally.
