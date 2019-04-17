5576c3b6ce2f6f0214ea70fafbe190d86b46c5f5
Neucrack <czd666666@gmail.com>
2019-04-17 10:14:59 +0800

    remove clock module, move function to time module

--- ports/k210-freertos/mpy_support/mpconfigport.h
+++ ports/k210-freertos/mpy_support/mpconfigport.h
@@ -253,7 +253,6 @@ extern const struct _mp_obj_module_t socket_module;
 extern const struct _mp_obj_module_t image_module;
 extern const struct _mp_obj_module_t sensor_module;
 extern const struct _mp_obj_module_t lcd_module;
-extern const struct _mp_obj_module_t time_module;
 extern const struct _mp_obj_module_t cpufreq_module;
 extern const struct _mp_obj_module_t kpu_module;
 extern const struct _mp_obj_module_t audio_module;
@@ -332,7 +331,6 @@ extern const struct _mp_obj_module_t mp_module_touchscreen;
     { MP_OBJ_NEW_QSTR(MP_QSTR_image), (mp_obj_t)&image_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_sensor), (mp_obj_t)&sensor_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_lcd), (mp_obj_t)&lcd_module }, \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_clock), (mp_obj_t)&time_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_cpufreq), (mp_obj_t)&cpufreq_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_KPU), (mp_obj_t)&kpu_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_audio), (mp_obj_t)&audio_module }, \
--- ports/k210-freertos/mpy_support/standard_lib/utime/modutime.c
+++ ports/k210-freertos/mpy_support/standard_lib/utime/modutime.c
@@ -42,6 +42,13 @@
 #include "extmod/utime_mphal.h"
 #include "lib/timeutils/timeutils.h"
 
+/* Clock Type */
+typedef struct _py_clock_obj_t {
+    mp_obj_base_t base;
+    uint32_t t_start;
+    uint32_t t_ticks;
+    uint32_t t_frame;
+} py_clock_obj_t;
 
 STATIC mp_uint_t timeutils_seconds_since_1970(mp_uint_t year, mp_uint_t month,
     mp_uint_t date, mp_uint_t hour, mp_uint_t minute, mp_uint_t second) {
@@ -132,6 +139,94 @@ STATIC mp_obj_t time_set_time(mp_obj_t tuple) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(time_set_time_obj, time_set_time);
 
+/////////////////////////////////////////////////////////////////////////////
+// For Openmv compatible
+mp_obj_t py_clock_tick(mp_obj_t clock_obj)
+{
+    py_clock_obj_t *clock = (py_clock_obj_t*) clock_obj;
+    clock->t_start = systick_current_millis();
+    return mp_const_none;
+}
+
+mp_obj_t py_clock_fps(mp_obj_t clock_obj)
+{
+    py_clock_obj_t *clock = (py_clock_obj_t*) clock_obj;
+    clock->t_frame++;
+    clock->t_ticks += (systick_current_millis()-clock->t_start);
+    float fps = 1000.0f / (clock->t_ticks/(float)clock->t_frame);
+    if (clock->t_ticks >= 2000) {
+        // Reset the FPS clock every 2s
+        clock->t_frame = 0;
+        clock->t_ticks = 0;
+    }
+    return mp_obj_new_float(fps);
+}
+
+mp_obj_t py_clock_avg(mp_obj_t clock_obj)
+{
+    py_clock_obj_t *clock = (py_clock_obj_t*) clock_obj;
+    clock->t_frame++;
+    clock->t_ticks += (systick_current_millis()-clock->t_start);
+    return mp_obj_new_float(clock->t_ticks/(float)clock->t_frame);
+}
+
+mp_obj_t py_clock_reset(mp_obj_t clock_obj)
+{
+    py_clock_obj_t *clock = (py_clock_obj_t*) clock_obj;
+    clock->t_start = 0;
+    clock->t_ticks = 0;
+    clock->t_frame = 0;
+    return mp_const_none;
+}
+
+static void py_clock_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind)
+{
+    py_clock_obj_t *self = self_in;
+
+    /* print some info */
+    mp_printf(print, "t_start:%d t_ticks:%d t_frame:%d\n",
+            self->t_start, self->t_ticks, self->t_frame);
+}
+
+static MP_DEFINE_CONST_FUN_OBJ_1(py_clock_tick_obj,  py_clock_tick);
+static MP_DEFINE_CONST_FUN_OBJ_1(py_clock_fps_obj,   py_clock_fps);
+static MP_DEFINE_CONST_FUN_OBJ_1(py_clock_avg_obj,   py_clock_avg);
+static MP_DEFINE_CONST_FUN_OBJ_1(py_clock_reset_obj, py_clock_reset);
+
+static const mp_map_elem_t locals_dict_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR_tick),   (mp_obj_t)&py_clock_tick_obj},
+    { MP_OBJ_NEW_QSTR(MP_QSTR_fps),    (mp_obj_t)&py_clock_fps_obj},
+    { MP_OBJ_NEW_QSTR(MP_QSTR_avg),    (mp_obj_t)&py_clock_avg_obj},
+    { MP_OBJ_NEW_QSTR(MP_QSTR_reset),  (mp_obj_t)&py_clock_reset_obj},
+    { NULL, NULL },
+};
+
+STATIC MP_DEFINE_CONST_DICT(locals_dict, locals_dict_table);
+
+static const mp_obj_type_t py_clock_type = {
+    { &mp_type_type },
+    .name  = MP_QSTR_Clock,
+    .print = py_clock_print,
+    .locals_dict = (mp_obj_t)&locals_dict,
+};
+
+
+static mp_obj_t py_time_clock()
+{
+    py_clock_obj_t *clock =NULL;
+    clock = m_new_obj(py_clock_obj_t);
+    clock->base.type = &py_clock_type;
+    clock->t_start = 0;
+    clock->t_ticks = 0;
+    clock->t_frame = 0;
+
+    return clock;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(py_time_clock_obj, py_time_clock);
+// For Openmv compatible end
+/////////////////////////////////////////////////////////////////////////////
+
 STATIC const mp_rom_map_elem_t time_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_utime) },
 
@@ -147,6 +242,11 @@ STATIC const mp_rom_map_elem_t time_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_ticks_cpu), MP_ROM_PTR(&mp_utime_ticks_cpu_obj) },
     { MP_ROM_QSTR(MP_QSTR_ticks_add), MP_ROM_PTR(&mp_utime_ticks_add_obj) },
     { MP_ROM_QSTR(MP_QSTR_ticks_diff), MP_ROM_PTR(&mp_utime_ticks_diff_obj) },
+
+    // openmv compatible
+    { MP_OBJ_NEW_QSTR(MP_QSTR_ticks),   MP_ROM_PTR(&mp_utime_ticks_ms_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_clock),   MP_ROM_PTR(&py_time_clock_obj) },
+
 };
 
 STATIC MP_DEFINE_CONST_DICT(time_module_globals, time_module_globals_table);
