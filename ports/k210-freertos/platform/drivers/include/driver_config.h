518c3d5b27f70428274832379401ccd3b0b5c175
Neucrack <czd666666@gmail.com>
2019-06-12 10:57:01 +0800

    change default sensor size to QVGA for m5stick

--- ports/k210-freertos/platform/drivers/include/driver_config.h
+++ ports/k210-freertos/platform/drivers/include/driver_config.h
@@ -4,15 +4,18 @@
 
 ////////// LCD ////////////
 #ifdef MAIXPY_M5STICK
-    #define LCD_W_MAX 240
-    #define LCD_H_MAX 135
+    #define LCD_W_MAX 320
+    #define LCD_H_MAX 240
+    #define LCD_W     240
+    #define LCD_H     135
 #else
     #define LCD_W_MAX 320
     #define LCD_H_MAX 240
+    #define LCD_W     LCD_W_MAX
+    #define LCD_H     LCD_H_MAX
 #endif
 
-#define LCD_W     LCD_W_MAX
-#define LCD_H     LCD_H_MAX
+
 ///////////////////////////
 
 
