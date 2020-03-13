d3e71c0a1d7ad64e96061aa9a929bf8b06e9af45
Neucrack <CZD666666@gmail.com>
2020-03-13 12:14:14 +0800

    add version and url for boot image

--- components/micropython/port/builtin_py/_boot.py
+++ components/micropython/port/builtin_py/_boot.py
@@ -53,7 +53,10 @@ test_pin=16
 fm.fpioa.set_function(test_pin,FPIOA.GPIO7)
 test_gpio=GPIO(GPIO.GPIO7,GPIO.PULL_UP)
 lcd.init(color=(255,0,0))
-lcd.draw_string(lcd.width()//2-68,lcd.height()//2-4, "Welcome to MaixPy", lcd.WHITE, lcd.RED)
+lcd.draw_string(lcd.width()//2-68,lcd.height()//2-24, "Welcome to MaixPy", lcd.WHITE, lcd.RED)
+v = sys.implementation.version
+lcd.draw_string(lcd.width()//2-70,lcd.height()//2+12, 'V{}.{}.{} : maixpy.io'.format(v[0],v[1],v[2]), lcd.WHITE, lcd.RED)
+del v
 if test_gpio.value() == 0:
     print('PIN 16 pulled down, enter test mode')
     lcd.clear(lcd.PINK)
