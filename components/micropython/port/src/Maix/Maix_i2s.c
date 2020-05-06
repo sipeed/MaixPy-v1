41586282fb5df96f314d29fe380fb734b993a5fd
junhuanchen <junhuanchen@qq.com>
2020-05-06 17:22:51 +0800

    Maix_i2s_record change No-Block, add wait_record.

--- components/micropython/port/src/Maix/Maix_i2s.c
+++ components/micropython/port/src/Maix/Maix_i2s.c
@@ -270,10 +270,17 @@ STATIC mp_obj_t Maix_i2s_record(size_t n_args, const mp_obj_t *pos_args, mp_map_
 
     //record 
     i2s_receive_data_dma(self->i2s_num, audio_obj->audio.buf, audio_obj->audio.points , DMAC_CHANNEL5);
-    dmac_wait_idle(DMAC_CHANNEL5);//wait to finish recv
+    // dmac_wait_idle(DMAC_CHANNEL5);//wait to finish recv
     return MP_OBJ_FROM_PTR(audio_obj);
 }
 MP_DEFINE_CONST_FUN_OBJ_KW(Maix_i2s_record_obj,1,Maix_i2s_record);
+
+STATIC mp_obj_t Maix_i2s_wait_record(void*self_)
+{
+    dmac_wait_idle(DMAC_CHANNEL5);//wait to finish recv
+    return mp_const_none;
+}
+MP_DEFINE_CONST_FUN_OBJ_1(Maix_i2s_wait_record_obj, Maix_i2s_wait_record);
 
 STATIC mp_obj_t Maix_i2s_play(void*self_, mp_obj_t audio_obj)
 {
@@ -303,6 +310,7 @@ STATIC const mp_rom_map_elem_t Maix_i2s_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_channel_config),  MP_ROM_PTR(&Maix_i2s_channel_config_obj) },
     { MP_ROM_QSTR(MP_QSTR_set_sample_rate), MP_ROM_PTR(&Maix_i2s_set_sample_rate_obj) },
     { MP_ROM_QSTR(MP_QSTR_record),          MP_ROM_PTR(&Maix_i2s_record_obj) },
+    { MP_ROM_QSTR(MP_QSTR_wait_record),          MP_ROM_PTR(&Maix_i2s_wait_record_obj) },
     { MP_ROM_QSTR(MP_QSTR_play),            MP_ROM_PTR(&Maix_i2s_play_obj) },
     //advance interface , some user don't use it
     // { MP_ROM_QSTR(MP_QSTR_set_dma_divede_16), MP_ROM_PTR(&Maix_i2s_set_dma_divede_16_obj) },
