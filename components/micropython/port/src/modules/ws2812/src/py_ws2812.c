22ab99ea284a63394f6e1123e90af6cb7d4c2ea6
junhuanchen <junhuanchen@qq.com>
2020-10-13 17:29:40 +0800

    [py_ws2812] add ws_num() get ws_num.

--- components/micropython/port/src/modules/ws2812/src/py_ws2812.c
+++ components/micropython/port/src/modules/ws2812/src/py_ws2812.c
@@ -182,10 +182,17 @@ STATIC mp_obj_t modules_ws2812_dis(mp_obj_t self_in) {
 }
 MP_DEFINE_CONST_FUN_OBJ_1(modules_ws2812_dis_obj, modules_ws2812_dis);
 
+STATIC mp_obj_t modules_ws2812_num(mp_obj_t self_in) {
+    modules_ws2812_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_int(self->dat->ws_num);
+}
+MP_DEFINE_CONST_FUN_OBJ_1(modules_ws2812_num_obj, modules_ws2812_num);
+
 STATIC const mp_rom_map_elem_t mp_modules_ws2812_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&modules_ws2812_del_obj) },
     { MP_ROM_QSTR(MP_QSTR_set_led), MP_ROM_PTR(&modules_ws2812_set_led_obj) },
     { MP_ROM_QSTR(MP_QSTR_display), MP_ROM_PTR(&modules_ws2812_dis_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ws_num), MP_ROM_PTR(&modules_ws2812_num_obj) },
 };
 
 MP_DEFINE_CONST_DICT(mp_modules_ws2812_locals_dict, mp_modules_ws2812_locals_dict_table);
