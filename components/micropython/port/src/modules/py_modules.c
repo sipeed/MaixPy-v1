e460e408f715cfc9915429205b0a3a4358e473a8
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2020-10-18 02:52:33 +0200

    add support amg88xx

--- components/micropython/port/src/modules/py_modules.c
+++ components/micropython/port/src/modules/py_modules.c
@@ -19,6 +19,9 @@ STATIC const mp_map_elem_t modules_globals_table[] = {
 #if CONFIG_MAIXPY_HTPA_ENABLE
     { MP_ROM_QSTR(MP_QSTR_htpa),  MP_ROM_PTR(&modules_htpa_type) },
 #endif
+#if CONFIG_MAIXPY_AMG88XX_ENABLE
+    { MP_ROM_QSTR(MP_QSTR_amg88xx),  MP_ROM_PTR(&modules_amg88xx_type) },
+#endif
 #if CONFIG_MAIXPY_SPEECH_RECOGNIZER_ENABLE
     { MP_ROM_QSTR(MP_QSTR_SpeechRecognizer),  MP_ROM_PTR(&modules_speech_recognizer_type) },
 #endif
