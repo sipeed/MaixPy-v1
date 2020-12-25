7962bda89b95462f6ae75127e9952d47fac2010a
QinYUN575 <QinYUN575@Gmail.com>
2020-12-25 10:33:16 +0800

    [amigo sdcard]Separate the amigo sd configuration definition

--- projects/maixpy_amigo_tft/main/src/main.cpp
+++ projects/maixpy_amigo_tft/main/src/main.cpp
@@ -1,8 +1,12 @@
 #include "maixpy.h"
-void* __dso_handle=0;
+#include "sdcard.h"
+void * __dso_handle = 0 ;
+
+extern sdcard_config_t config;
 int main()
 {
+    sdcard_config_t amigo = { 10, 6, 11, 26, SD_CS_PIN };
+    config = amigo;
     maixpy_main();
     return 0;
 }
-
