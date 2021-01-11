f18990aa32d6c7a9ed609c7a790a02c40479d1ad
junhuanchen <junhuanchen@qq.com>
2021-01-11 09:36:47 +0800

    [amigo firmware] Auto switches config.json when start.

--- projects/maixpy_amigo_ips/builtin_py/_boot.py
+++ projects/maixpy_amigo_ips/builtin_py/_boot.py
@@ -106,7 +106,7 @@ except Exception:
     pass
 
 banner = '''
- __  __              _____  __   __  _____   __     __
+__  __              _____  __   __  _____   __     __
 |  \/  |     /\     |_   _| \ \ / / |  __ \  \ \   / /
 | \  / |    /  \      | |    \ V /  | |__) |  \ \_/ /
 | |\/| |   / /\ \     | |     > <   |  ___/    \   /
@@ -120,72 +120,69 @@ print(banner)
 del banner
 
 try:
-  from machine import I2C
-  axp173 = I2C(I2C.I2C3, freq=100000, scl=24, sda=27)
-  axp173.writeto_mem(0x34, 0x27, 0x20, mem_size=8)
-  axp173.writeto_mem(0x34, 0x28, 0x0C, mem_size=8)
-  axp173.writeto_mem(0x34, 0x36, 0xCC, mem_size=8)
-  del axp173
+    from machine import I2C
+    axp173 = I2C(I2C.I2C3, freq=100000, scl=24, sda=27)
+    axp173.writeto_mem(0x34, 0x27, 0x20, mem_size=8)
+    axp173.writeto_mem(0x34, 0x28, 0x0C, mem_size=8)
+    axp173.writeto_mem(0x34, 0x36, 0xCC, mem_size=8)
+    del axp173
 except Exception as e:
-  print(e)
+    print(e)
+
+import json
+
+config = {
+"type": "amigo_ips",
+"lcd": {
+    "height": 320,
+    "width": 480,
+    "invert": 1,
+    "dir": 40,
+    "lcd_type": 2
+},
+"sdcard":{
+    "sclk":11,
+    "mosi":10,
+    "miso":6,
+    "cs":26
+},
+"board_info": {
+    'BOOT_KEY': 16,
+    'LED_R': 14,
+    'LED_G': 15,
+    'LED_B': 17,
+    'LED_W': 32,
+    'BACK': 31,
+    'ENTER': 23,
+    'NEXT': 20,
+    'WIFI_TX': 6,
+    'WIFI_RX': 7,
+    'WIFI_EN': 8,
+    'I2S0_MCLK': 13,
+    'I2S0_SCLK': 21,
+    'I2S0_WS': 18,
+    'I2S0_IN_D0': 35,
+    'I2S0_OUT_D2': 34,
+    'I2C_SDA': 27,
+    'I2C_SCL': 24,
+    'SPI_SCLK': 11,
+    'SPI_MOSI': 10,
+    'SPI_MISO': 6,
+    'SPI_CS': 12,
+}
+}
+
+cfg = json.dumps(config)
+print(cfg)
 
 try:
-  os.stat('/flash/config.json')
-except OSError as e:
-  import json
-
-  config = {
-    "type": "amigo_ips",
-    "lcd": {
-        "height": 320,
-        "width": 480,
-        "invert": 1,
-        "dir": 40,
-        "lcd_type": 2
-    },
-    "sdcard":{
-        "sclk":11,
-        "mosi":10,
-        "miso":6,
-        "cs":26
-    },
-    "board_info": {
-        'BOOT_KEY': 16,
-        'LED_R': 14,
-        'LED_G': 15,
-        'LED_B': 17,
-        'LED_W': 32,
-        'BACK': 31,
-        'ENTER': 23,
-        'NEXT': 20,
-        'WIFI_TX': 6,
-        'WIFI_RX': 7,
-        'WIFI_EN': 8,
-        'I2S0_MCLK': 13,
-        'I2S0_SCLK': 21,
-        'I2S0_WS': 18,
-        'I2S0_IN_D0': 35,
-        'I2S0_OUT_D2': 34,
-        'I2C_SDA': 27,
-        'I2C_SCL': 24,
-        'SPI_SCLK': 11,
-        'SPI_MOSI': 10,
-        'SPI_MISO': 6,
-        'SPI_CS': 12,
-    }
-  }
-
-  cfg = json.dumps(config)
-  print(cfg)
-
-  try:
     with open('/flash/config.json', 'rb') as f:
-      tmp = json.loads(f.read())
-      print(tmp)
-      if tmp["type"] != config["type"]:
-        raise Exception('config.json no exist')
-  except Exception as e:
+        tmp = json.loads(f.read())
+        # print(tmp)
+        if tmp["type"] != config["type"]:
+            raise Exception('config.json no exist')
+except Exception as e:
     with open('/flash/config.json', "w") as f:
-      f.write(cfg)
+        f.write(cfg)
     import machine
     machine.reset()
--- projects/maixpy_amigo_tft/builtin_py/_boot.py
+++ projects/maixpy_amigo_tft/builtin_py/_boot.py
@@ -129,63 +129,60 @@ try:
 except Exception as e:
   print(e)
 
+import json
+
+config = {
+"type": "amigo_tft",
+"lcd": {
+    "height": 320,
+    "width": 480,
+    "invert": 0,
+    "dir": 40,
+    "lcd_type": 1
+},
+"sdcard":{
+    "sclk":11,
+    "mosi":10,
+    "miso":6,
+    "cs":26
+},
+"board_info": {
+    'BOOT_KEY': 23,
+    'LED_R': 14,
+    'LED_G': 15,
+    'LED_B': 17,
+    'LED_W': 32,
+    'BACK': 31,
+    'ENTER': 23,
+    'NEXT': 20,
+    'WIFI_TX': 6,
+    'WIFI_RX': 7,
+    'WIFI_EN': 8,
+    'I2S0_MCLK': 13,
+    'I2S0_SCLK': 21,
+    'I2S0_WS': 18,
+    'I2S0_IN_D0': 35,
+    'I2S0_OUT_D2': 34,
+    'I2C_SDA': 27,
+    'I2C_SCL': 24,
+    'SPI_SCLK': 11,
+    'SPI_MOSI': 10,
+    'SPI_MISO': 6,
+    'SPI_CS': 12,
+}
+}
+
+cfg = json.dumps(config)
+print(cfg)
+
 try:
-  os.stat('/flash/config.json')
-except OSError as e:
-  import json
-
-  config = {
-    "type": "amigo_tft",
-    "lcd": {
-        "height": 320,
-        "width": 480,
-        "invert": 0,
-        "dir": 40,
-        "lcd_type": 1
-    },
-    "sdcard":{
-        "sclk":11,
-        "mosi":10,
-        "miso":6,
-        "cs":26
-    },
-    "board_info": {
-        'BOOT_KEY': 23,
-        'LED_R': 14,
-        'LED_G': 15,
-        'LED_B': 17,
-        'LED_W': 32,
-        'BACK': 31,
-        'ENTER': 23,
-        'NEXT': 20,
-        'WIFI_TX': 6,
-        'WIFI_RX': 7,
-        'WIFI_EN': 8,
-        'I2S0_MCLK': 13,
-        'I2S0_SCLK': 21,
-        'I2S0_WS': 18,
-        'I2S0_IN_D0': 35,
-        'I2S0_OUT_D2': 34,
-        'I2C_SDA': 27,
-        'I2C_SCL': 24,
-        'SPI_SCLK': 11,
-        'SPI_MOSI': 10,
-        'SPI_MISO': 6,
-        'SPI_CS': 12,
-    }
-  }
-
-  cfg = json.dumps(config)
-  print(cfg)
-
-  try:
     with open('/flash/config.json', 'rb') as f:
-      tmp = json.loads(f.read())
-      print(tmp)
-      if tmp["type"] != config["type"]:
+        tmp = json.loads(f.read())
+    # print(tmp)
+    if tmp["type"] != config["type"]:
         raise Exception('config.json no exist')
-  except Exception as e:
+except Exception as e:
     with open('/flash/config.json', "w") as f:
-      f.write(cfg)
+        f.write(cfg)
     import machine
     machine.reset()
