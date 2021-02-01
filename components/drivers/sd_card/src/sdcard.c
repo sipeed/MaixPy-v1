0bd0bea55426aba01eea4fcc9117de4c46d23420
vamoosebbf <vamoosebbf@qq,com>
2021-02-01 13:37:38 +0800

    [sdcard] fix misaligned error

--- components/drivers/sd_card/src/sdcard.c
+++ components/drivers/sd_card/src/sdcard.c
@@ -100,13 +100,15 @@ static void sd_read_data(uint8_t *data_buff, uint32_t length)
 
 static void sd_write_data_dma(uint8_t *data_buff)
 {
-    spi_init(SD_SPI_DEVICE, SPI_WORK_MODE_0, SPI_FF_STANDARD, 32, 1);
+    // spi_init(SD_SPI_DEVICE, SPI_WORK_MODE_0, SPI_FF_STANDARD, 32, 1); misaligned error
+    spi_init(SD_SPI_DEVICE, SPI_WORK_MODE_0, SPI_FF_STANDARD, 8, 0);
     spi_send_data_standard_dma(SD_DMA_CH, SD_SPI_DEVICE, SD_SS, NULL, 0, (uint8_t *)(data_buff), 128 * 4);
 }
 
 static void sd_read_data_dma(uint8_t *data_buff)
 {
-    spi_init(SD_SPI_DEVICE, SPI_WORK_MODE_0, SPI_FF_STANDARD, 32, 1);
+    // spi_init(SD_SPI_DEVICE, SPI_WORK_MODE_0, SPI_FF_STANDARD, 32, 1); misaligned error
+    spi_init(SD_SPI_DEVICE, SPI_WORK_MODE_0, SPI_FF_STANDARD, 8, 0);
     spi_receive_data_standard_dma(-1, SD_DMA_CH, SD_SPI_DEVICE, SD_SS, NULL, 0, data_buff, 128 * 4);
 }
 
