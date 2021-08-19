aecd4d86a0f0c21c37da5a30cdb7701ed4178043
alan840706 <58114247+alan840706@users.noreply.github.com>
2021-08-20 00:28:58 +0800

    Update machine_spi.c

--- components/micropython/port/src/standard_lib/machine/machine_spi.c
+++ components/micropython/port/src/standard_lib/machine/machine_spi.c
@@ -520,8 +520,8 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_spi_deinit_obj, machine_spi_deinit);
 STATIC void mp_machine_spi_transfer(mp_obj_t self, size_t len, const void *src, void *dest, int cs) {
     mp_obj_base_t *s = (mp_obj_base_t*)MP_OBJ_TO_PTR(self);
     mp_machine_hw_spi_p_t *spi_p = (mp_machine_hw_spi_p_t*)s->type->protocol;
-    spi_p->transfer(s, len, src, dest, cs);
     printf("Alan");
+    spi_p->transfer(s, len, src, dest, cs);
 }
 
 STATIC mp_obj_t mp_machine_spi_read(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {	
