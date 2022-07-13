d1e86c6e4dc1920d6c57c3527d91867814f5b3f8
odudex <eduardo.schoenknecht@gmail.com>
2022-07-13 20:57:40 -0300

    Maix Dock Support (#4)
    * Power button as IO for M5stickV, extended ASCII for Amigo and Bit
    
    * Update board.py
    
    - Changed buttons order to help assembly
    - Added board button as 'page_prev'
    
    * kflash Kconfiglib mkspiffs submodules updated
    
    * Maix dock support
    
    * Fix. Dock has no lenses
    
    * Underclocked Amigo to 300MHz to run cooler
    Amigo has poor heat dissipation
    No noticeble perfonce loss

---
+++ projects/maixpy_dock/builtin_py/board.py
@@ -0,0 +1,46 @@
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+config = json.loads("""
+{
+    "type":"bit",
+    "lcd": {
+        "height": 240,
+        "width": 320,
+        "invert": 0,
+        "lcd_type": 0
+    },
+    "sdcard": {
+        "sclk": 27,
+        "mosi": 28,
+        "miso": 26,
+        "cs": 29
+    },
+    "board_info": {
+      "BOOT_KEY": 16,
+      "LED_R": 13,
+      "LED_G": 12,
+      "LED_B": 14,
+      "MIC0_WS": 19,
+      "MIC0_DATA": 20,
+      "MIC0_BCK": 18
+    },
+    "krux": {
+        "pins":{
+            "BUTTON_A": 9,
+            "ENCODER": [10, 11]
+        },
+        "display": {
+            "touch": false,
+            "font": [8, 16],
+            "orientation":[1, 0]
+        },
+        "sensor": {
+            "flipped": true,
+            "lenses": false
+        }
+    }
+}
+""")
---
+++ projects/maixpy_dock/main/src/main.cpp
@@ -0,0 +1,10 @@
+#include "maixpy.h"
+
+void * __dso_handle = 0 ;
+
+int main()
+{
+    maixpy_main();
+    return 0;
+}
+
