aee8339e0761f98c374c7a0beaa3aa4b4df26818
odudex <odudex@proton.me>
2023-11-12 13:29:44 -0300

    add method to draw binary img QR codes

--- components/micropython/port/src/omv/py/py_lcd.c
+++ components/micropython/port/src/omv/py/py_lcd.c
@@ -722,6 +722,7 @@ STATIC mp_obj_t py_lcd_fill_rectangle(size_t n_args, const mp_obj_t *args)
 }
 
 extern void imlib_set_pixel(image_t *img, int x, int y, int p);
+
 STATIC mp_obj_t py_lcd_draw_qr_code(size_t n_args, const mp_obj_t *args)
 {
     if (lcd_para.width == 0 || lcd_para.width  == 0)
@@ -851,6 +852,129 @@ STATIC mp_obj_t py_lcd_draw_qr_code(size_t n_args, const mp_obj_t *args)
     return mp_const_none;
 }
 
+STATIC mp_obj_t py_lcd_draw_qr_code_binary(size_t n_args, const mp_obj_t *args) {
+    if (lcd_para.width == 0 || lcd_para.width == 0)
+        mp_raise_msg(&mp_type_ValueError, "not init");
+
+    uint16_t y0 = mp_obj_get_int(args[0]);
+    mp_obj_t binary_qr_obj = args[1];
+    mp_buffer_info_t qr_bufinfo;
+    mp_get_buffer_raise(binary_qr_obj, &qr_bufinfo, MP_BUFFER_READ);
+    uint8_t* qr_data = qr_bufinfo.buf;
+
+    if (qr_bufinfo.len == 0)
+        return mp_const_none;
+
+    uint16_t max_width = mp_obj_get_int(args[2]);
+
+    uint16_t dark_color = BLACK;
+    uint16_t light_color = WHITE;
+    uint16_t bg_color = BLACK;
+
+    if (n_args >= 4) {
+        mp_obj_t *arg_color;
+
+        if (mp_obj_is_integer(args[3])) {
+            dark_color = mp_obj_get_int(args[3]);
+        } else {
+            mp_obj_get_array_fixed_n(args[3], 3, &arg_color);
+            dark_color = COLOR_R8_G8_B8_TO_RGB565(IM_MAX(IM_MIN(mp_obj_get_int(arg_color[0]), COLOR_R8_MAX), COLOR_R8_MIN),
+                                                IM_MAX(IM_MIN(mp_obj_get_int(arg_color[1]), COLOR_G8_MAX), COLOR_G8_MIN),
+                                                IM_MAX(IM_MIN(mp_obj_get_int(arg_color[2]), COLOR_B8_MAX), COLOR_B8_MIN));
+        }
+
+        if (n_args >= 5) {
+            if (mp_obj_is_integer(args[4])) {
+                light_color = mp_obj_get_int(args[4]);
+            } else {
+                mp_obj_get_array_fixed_n(args[4], 3, &arg_color);
+                light_color = COLOR_R8_G8_B8_TO_RGB565(IM_MAX(IM_MIN(mp_obj_get_int(arg_color[0]), COLOR_R8_MAX), COLOR_R8_MIN),
+                                                    IM_MAX(IM_MIN(mp_obj_get_int(arg_color[1]), COLOR_G8_MAX), COLOR_G8_MIN),
+                                                    IM_MAX(IM_MIN(mp_obj_get_int(arg_color[2]), COLOR_B8_MAX), COLOR_B8_MIN));
+            }
+
+            if (n_args >= 6) {
+                if (mp_obj_is_integer(args[5])) {
+                    bg_color = mp_obj_get_int(args[5]);
+                } else {
+                    mp_obj_get_array_fixed_n(args[5], 3, &arg_color);
+                    bg_color = COLOR_R8_G8_B8_TO_RGB565(IM_MAX(IM_MIN(mp_obj_get_int(arg_color[0]), COLOR_R8_MAX), COLOR_R8_MIN),
+                                                        IM_MAX(IM_MIN(mp_obj_get_int(arg_color[1]), COLOR_G8_MAX), COLOR_G8_MIN),
+                                                        IM_MAX(IM_MIN(mp_obj_get_int(arg_color[2]), COLOR_B8_MAX), COLOR_B8_MIN));
+                }
+            }
+        }
+    }
+
+    int starting_size = (int)sqrt(qr_bufinfo.len * 8);
+    int block_size_divisor = starting_size + 2;  // adds 2 to create room for a 1 block border
+    int scale = max_width / block_size_divisor;
+    int width = starting_size * scale;
+    int border_size = (max_width - width) / 2;
+    int opposite_border_offset = border_size + width - 1;
+
+    uint8_t *pixels = NULL;
+    pixels = (uint8_t *)malloc(max_width * max_width * 2);
+    if (!pixels)
+        mp_raise_OSError(MP_ENOMEM);
+
+    image_t arg_img = {
+        .bpp = IMAGE_BPP_RGB565,
+        .w = max_width,
+        .h = max_width,
+        .pixels = pixels
+    };
+
+    // Paint a frame, needed when animated QR codes have variable sizes
+    // Top border
+    for (int rx = 0; rx < max_width; rx++) {
+        for (int ry = 0; ry < border_size; ry++) {
+            imlib_set_pixel(&arg_img, rx, ry, bg_color);
+        }
+    }
+    // Bottom border
+    for (int rx = 0; rx < max_width; rx++) {
+        for (int ry = opposite_border_offset; ry < max_width; ry++) {
+            imlib_set_pixel(&arg_img, rx, ry, bg_color);
+        }
+    }
+    // Left border
+    for (int rx = 0; rx < border_size; rx++) {
+        for (int ry = border_size; ry < opposite_border_offset; ry++) {
+            imlib_set_pixel(&arg_img, rx, ry, bg_color);
+        }
+    }
+    // Right border
+    for (int rx = opposite_border_offset; rx < max_width; rx++) {
+        for (int ry = border_size; ry < opposite_border_offset; ry++) {
+            imlib_set_pixel(&arg_img, rx, ry, bg_color);
+        }
+    }
+
+    // QR code rendering
+    for (int og_y = 0; og_y < starting_size; og_y++) {
+        for (int og_x = 0; og_x < starting_size; og_x++) {
+            int og_yx_index = og_y * starting_size + og_x;
+            uint16_t color;
+            uint8_t color_byte = qr_data[og_yx_index >> 3];
+            color_byte &= (1 << (og_yx_index % 8));
+            color = color_byte ? dark_color : light_color;
+            for (int i = 0; i < scale; i++) {
+                int y = border_size + og_y * scale + i;
+                for (int j = 0; j < scale; j++) {
+                    int x = border_size + og_x * scale + j;
+                    imlib_set_pixel(&arg_img, x, y, color);
+                }
+            }
+        }
+    }
+
+    lcd->draw_picture(0, y0, max_width, max_width, (uint8_t *)pixels);
+    free(pixels);
+    return mp_const_none;
+}
+
+
 STATIC mp_obj_t py_lcd_freq(size_t n_args, const mp_obj_t *pos_args)
 {
     mp_int_t freq = lcd->get_freq();
@@ -880,6 +1004,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(py_lcd_bgr_to_rgb_obj, 0, 1, py_lcd_b
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(py_lcd_draw_string_obj, 3, 5, py_lcd_draw_string);
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(py_lcd_fill_rectangle_obj, 3, 5, py_lcd_fill_rectangle);
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(py_lcd_draw_qr_code_obj, 3, 6, py_lcd_draw_qr_code);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(py_lcd_draw_qr_code_binary_obj, 3, 6, py_lcd_draw_qr_code_binary);
 
 
 static const mp_map_elem_t globals_dict_table[] = {
@@ -901,6 +1026,7 @@ static const mp_map_elem_t globals_dict_table[] = {
     {MP_OBJ_NEW_QSTR(MP_QSTR_draw_string), (mp_obj_t)&py_lcd_draw_string_obj},
     {MP_OBJ_NEW_QSTR(MP_QSTR_fill_rectangle), (mp_obj_t)&py_lcd_fill_rectangle_obj},
     {MP_OBJ_NEW_QSTR(MP_QSTR_draw_qr_code), (mp_obj_t)&py_lcd_draw_qr_code_obj},
+    {MP_OBJ_NEW_QSTR(MP_QSTR_draw_qr_code_binary), (mp_obj_t)&py_lcd_draw_qr_code_binary_obj},
     {MP_OBJ_NEW_QSTR(MP_QSTR_register), (mp_obj_t)&py_lcd_write_register_obj},
     {MP_OBJ_NEW_QSTR(MP_QSTR_XY_RLUD), MP_OBJ_NEW_SMALL_INT(DIR_XY_RLUD)},
     {MP_OBJ_NEW_QSTR(MP_QSTR_YX_RLUD), MP_OBJ_NEW_SMALL_INT(DIR_YX_RLUD)},
